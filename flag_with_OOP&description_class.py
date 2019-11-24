80379a04d4577361079a4943ec1362e03ed3b142
Bogdankozlovskiy <bkozlovskiy@bk.ru>
2019-11-24 04:28:41 +0300

    add goob name

--- flag_with_OOP&description_class.py
+++ flag_with_OOP&description_class.py
@@ -18,35 +18,42 @@ class BuildFlag:
 
     def __init__(self, N):
         self.N = N
-        self.rows = self.N
-        self.columns = 3 * self.N // 2
-        self.end_row = N
-        self.start_row = N // 2
+        self.half_rows = self.N
+        self.half_columns = 3 * self.N // 2
+        self.start_row_draw_circle = N // 2
+        self.end_row_draw_circle = N
 
     def build_quarter_field(self):
         self.quarter_field = []
-        for row in range(self.rows):
-            self.quarter_field.append([" " for i in range(self.columns)])
+        for row in range(self.half_rows):
+            self.quarter_field.append([" " for i in range(self.half_columns)])
 
     def draw_border_quarter_circle(self):
-        for index, row in enumerate(range(self.start_row, self.end_row), 1):
-            self.quarter_field[row][self.columns - index] = '*'
+        rows_for_draw = range(
+            self.start_row_draw_circle,
+            self.end_row_draw_circle)
+        for index, row in enumerate(rows_for_draw, 1):
+            self.quarter_field[row][self.half_columns - index] = '*'
 
     def pour_angle_in_quarter_circle(self):
-        for index, row in enumerate(range(self.start_row+1, self.end_row), 1):
-            for column in range(self.columns - index, self.columns):
+        rows_for_pour = range(
+            self.start_row_draw_circle + 1,
+            self.end_row_draw_circle)
+        for index, row in enumerate(rows_for_pour, 1):
+            for column in range(self.half_columns - index, self.half_columns):
                 self.quarter_field[row][column] = 'O'
 
     def add_quarter_border_field(self):
         for row in self.quarter_field:
             row.insert(0, '#')
-        self.quarter_field.insert(0, ['#' for i in range(self.columns + 1)])
+        border = ['#' for i in range(self.half_columns + 1)]
+        self.quarter_field.insert(0, border)
 
     def build_flag_by_quarter_field(self):
         for row in self.quarter_field:
             row.extend(row[::-1])
         self.quarter_field.extend(self.quarter_field[::-1])
-        for index in range(self.rows * 2 + 2):
+        for index in range(self.half_rows * 2 + 2):
             self.quarter_field[index] = "".join(self.quarter_field[index])
         self.flag = "\n".join(self.quarter_field)
         self.quarter_field.clear()
